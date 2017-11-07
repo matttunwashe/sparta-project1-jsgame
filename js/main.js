71ab17ad1b55ac766f12979768ffdf322875ac16
Tech-A20 <Tech-A20@Tech-A20s-MacBook-Air.local>
2017-11-07 14:28:16 +0000

    clicker button moves left only

--- js/main.js
+++ js/main.js
@@ -2,42 +2,53 @@ $( document ).ready(function() {
 
   console.log('working');
 
-$("#start").click( function(){
+  $("#start").click( function(){
 
-
-//the countdown timer
-  function countdown() {
-    var timer = setInterval(function() {
-    var count = parseInt($('#timer').html());
-    if (count !== 0) {
-      $('#timer').html(count - 1);
-    } else {
-      clearInterval(timer);
+    $(this).fadeOut();
+  //the countdown timer
+    function countdown() {
+      var timer = setInterval(function() {
+      var count = parseInt($('#timer').html());
+      if (count !== 0) {
+        $('#timer').html(count - 1);
+      } else {
+        clearInterval(timer);
+      }
+    }, 1000);
     }
-  }, 1000);
-}
 
-countdown();
+    countdown();
+
+    var x = 100;
+    var y = 100;
+    // var xd = 100;
+    // var yd = 100;
+
+  //clicker button increments
+    var score = 0;
+    $("#clicker").click(function () {
+      score = score + 1;
+      // console.log(score);
+      $("#count").html(score);
+
+      $(this).animate({
+        marginLeft: x + 'px'
+      })
+
+    })
+
+  //make the clicker move randomly
+  // var move = document.getElementById('#clicker');
+  //
+  // move.onclick=function(){
+  //   var x = Math.floor(Math.random()*1000);
+  //   var y = Math.floor(Math.random()*1000);
+  //   move.style.top = x + 'px';
+  //   move.style.left = y + 'px';
+  // };
 
 
-//clicker button increments
-  var score = 0;
-  $("#clicker").click(function () {
-    score = score + 1;
-    // console.log(score);
-    $("#count").html(score);
-  })
 
-//make the clicker move randomly
-var move = document.getElementById('#clicker');
-
-move.onclick=function(){
-  var x = Math.floor(Math.random()*1000);
-  var y = Math.floor(Math.random()*1000);
-  move.style.top = x + 'px';
-  move.style.left = y + 'px';
-};
-
-});
+  });
 
 })
