0ca2365869b0e5b314908e7427bb55441dab60cc
Tech-A20 <Tech-A20@Tech-A20s-MacBook-Air.local>
2017-11-07 11:56:48 +0000

    the countdown timer works

--- js/main.js
+++ js/main.js
@@ -2,13 +2,35 @@ $( document ).ready(function() {
 
   console.log('working');
 
+  function countdown() {
+    var timer = setInterval(function() {
+    var count = parseInt($('#timer').html());
+    if (count !== 0) {
+      $('#timer').html(count - 1);
+    } else {
+      clearInterval(timer);
+    }
+  }, 1000);
+}
+
+countdown();
 
 
 //clicker button increments
   var score = 0;
-  $("#start").click(function () {
+  $("#clicker").click(function () {
     score = score + 1;
-    console.log(score);
+    // console.log(score);
+    $("#count").html(score);
   })
 
 });
+
+
+
+//var count = document.getElementById('countdown');
+// timeoutfn = function(){
+//        count.innerHTML = parseInt(count.innerHTML) - 1;
+//        setTimeout(timeoutfn, 1000);
+// };
+// setTimeout(timeoutfn, 1000);
