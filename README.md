e5ebba50c224f1f600d4df8dca172e1357773a50
Tech-A20 <Tech-A20@Tech-A20s-MacBook-Air.local>
2017-11-09 21:15:20 +0000

    code indentation done

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# sparta-project1-jsgame
-This is how I coded my game "Fast Click!"
+# Rapid Clicker
+1. What is your game?
+  * This game is a 2 player game that test how fast you can click on the clicker. The clicker has been designed to move randomly which makes it more tricky to click.
+2. Introduction to your game.
+  * the
+3. Instructions how to play your game
+
+*Your Task*
+1. Your task (Could use requirements sheet in here)
+2. List of things your delivered (Completed user stories)
+3. What were you most pleased with
+4. What was the most challenging part
--- js/main.js
+++ js/main.js
@@ -1,135 +1,117 @@
 $( document ).ready(function() {
   //movement variables
-  var x = 100;
-  var y = 100;
-  var score = 0;
-  var score2 = 0;
-  var player1Turn = true;
-  var player2Turn = false;
-    $('#clicker2').hide();
+    var x = 100;
+    var y = 100;
+    var score = 0;
+    var score2 = 0;
+    var player1Turn = true;
+    var player2Turn = false;
+  //hides clicker2 from the start
+      $('#clicker2').hide();
   //setting the welcome message
   function welcomeMessage(){
     $('#introModal').show();
   }
-
-  $('#introModalClose').click(function(){
+    $('#introModalClose').click(function(){
     $('#introModal').hide();
   })
-
-  welcomeMessage();
-
+    welcomeMessage();
   //start button
-  $("#start").click( function(){
+    $("#start").click( function(){
   //start button fades out
     $(this).fadeOut();
-
-    function init() {
-      if(player1Turn === true ) {
-        countdown();
-        playerOne();
-        player1Turn = false;
+    //Calls the game's order
+  function init() {
+    if(player1Turn === true ) {
+      countdown();
+      playerOne();
+      player1Turn = false;
         $('#clicker').attr('id', 'clicker2');
         $('#clicker2').attr('id', 'clicker');
       } else {
         changePlayer();
-
         playerTwo();
         player2Turn = false;
-
       }
     }
-
     init();
-
-
-
   //the countdown timer
-    function countdown() {
-      var timer = setInterval(function() {
-      var count = parseInt($('#timer').html());
-        if (count !== 0) {
-          $('#timer').html(count - 1);
-        } else {
-          clearInterval(timer);
-          init();
-        }
-      }, 1000);
-    }
-
-    function changePlayer(){
-      $('#changePlayerModal').show();
-      $('#changePlayerModalClose').click(function(){
+  function countdown() {
+    var timer = setInterval(function() {
+    var count = parseInt($('#timer').html());
+    if (count !== 0) {
+        $('#timer').html(count - 1);
+      } else {
+        clearInterval(timer);
+        init();
+      }
+    }, 1000);
+  }
+  //display modal for player2 start
+  function changePlayer(){
+        $('#changePlayerModal').show();
+        $('#changePlayerModalClose').click(function(){
         $('#changePlayerModal').hide();
-        countdownTwo();
-      })
-    }
-
-
-
-
-    function countdownTwo() {
-      var timer2 = setInterval(function() {
-      var count2 = parseInt($('#timer2').html());
-        if (count2 !== 0) {
+      countdownTwo();
+    })
+  }
+  //start player2 countdown
+  function countdownTwo() {
+    var timer2 = setInterval(function() {
+    var count2 = parseInt($('#timer2').html());
+      if (count2 !== 0) {
           $('#timer2').html(count2 - 1);
         } else {
           clearInterval(timer2);
           checkForWin();
         }
-      }, 1000);
-    }
-
-
-    function playerOne(){
-      //clicker button increments
-
-      var scoreBoard = $('#score');
-
+    }, 1000);
+  }
+  //player1 moves 
+  function playerOne(){
+    var scoreBoard = $('#score');
       $("#clicker").click(function () {
         score = score + 1;
         scoreBoard.html(score);
-
-        //the clicker moves randomly
-        var x = Math.floor(Math.random()*725);
-        var y = Math.floor(Math.random()*1000);
-          $("#clicker").css('margin-top', (x + "px"))
-          $("#clicker").css('margin-left', (y + "px"))
-          $("#clicker").css('margin-right', (y + "px"))
-          $("#clicker").css('margin-bottom', (x + "px"))
-      });
-      // score = score;
-    }
-
-    function playerTwo() {
-      //clicker button increments
+  //clicker1 moves randomly
+    var x = Math.floor(Math.random()*725);
+    var y = Math.floor(Math.random()*1000);
+      $("#clicker").css('margin-top', (x + "px"))
+      $("#clicker").css('margin-left', (y + "px"))
+      $("#clicker").css('margin-right', (y + "px"))
+      $("#clicker").css('margin-bottom', (x + "px"))
+    });
+  }
+  //player2 move
+  function playerTwo() {
+  //clicker button increments
       $('#clicker').hide();
       $('#clicker2').show();
-      var scoreBoard = $('#score2');
-        $("#clicker2").click(function () {
-          score2 = score2 + 1;
-          scoreBoard.html(score2);
-
-        //the clicker moves randomly
-        var x = Math.floor(Math.random()*725);
-        var y = Math.floor(Math.random()*1000);
-          $("#clicker2").css('margin-top', (x + "px"))
-          $("#clicker2").css('margin-left', (y + "px"))
-          $("#clicker2").css('margin-right', (y + "px"))
-          $("#clicker2").css('margin-bottom', (x + "px"))
-      });
-    }
-    //this checks if it's a win or draw
-    function checkForWin() {
+    var scoreBoard = $('#score2');
+      $("#clicker2").click(function () {
+        score2 = score2 + 1;
+        scoreBoard.html(score2);
+  //clicker2 moves randomly
+    var x = Math.floor(Math.random()*725);
+    var y = Math.floor(Math.random()*1000);
+      $("#clicker2").css('margin-top', (x + "px"))
+      $("#clicker2").css('margin-left', (y + "px"))
+      $("#clicker2").css('margin-right', (y + "px"))
+      $("#clicker2").css('margin-bottom', (x + "px"))
+    });
+  }
+  //checks if it's a win or draw
+  function checkForWin() {
       if (score > score2){
         $('#player1WinModal').show();
         $('#player1WinModalClose').click(function(){
-          $('#player1WinModal').hide();
-        });
+        $('#player1WinModal').hide();
+      });
       } else if (score < score2){
         $('#player2WinModal').show();
         $('#player2WinModalClose').click(function(){
-          $('#player2WinModal').hide();
-        });
+        $('#player2WinModal').hide();
+      });
       } else if (score = score2) {
         $('#drawModal').show();
         $('#drawModalClose').click(function(){
